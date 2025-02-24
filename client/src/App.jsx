8047e81e5dee7b5659f87a71850b00ec60ab4ad4
andaemin <eoals0216@gmail.com>
2025-02-24 18:42:34 +0900

    feature/mainhome 및 components와 pages 구별해놓음 (별거안했음)

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Mainhome from './Pages/mainhome.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Mainhome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/twicemain.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Twicemain() {
+    return (
+        <div>
+            <p>twice pages</p>
+        </div>
+    );
+}
+
+export default Twicemain;
